03c588d7ac926fd2ed57f90bc72dfa621bd38534
Yuriy Poralo <mrbighokage@gmail.com>
2018-09-08 10:29:10 +0300

    Add connect to trades WebSocket combined

---
+++ BinanceExchange.API/Models/WebSocket/BinanceCombinedTradeData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace BinanceExchange.API.Models.WebSocket.Interfaces
+{
+    [DataContract]
+    public class BinanceCombinedTradeData : IWebSocketResponse
+    {
+        [DataMember(Order = 1)]
+        [JsonProperty(PropertyName = "stream")]
+        public string Stream { get; set; }
+
+        [DataMember(Order = 2)]
+        [JsonProperty(PropertyName = "data")]
+        public BinanceTradeOrderData Data { get; set; }
+
+        public string EventType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime EventTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
